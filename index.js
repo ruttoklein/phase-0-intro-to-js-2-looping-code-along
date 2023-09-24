0af97a7bd7c017b61748584205b4f64ea99edf6e
ruttklein <klein.rutto@student.moringaschool.com>
2023-09-24 23:17:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function countDown(n) {
+    let i = n;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  function writeCards(names) {
+    let thankYouMessages = [];
+    for (let name of names) {
+      thankYouMessages.push(Thank you, ${name}, for the wonderful surprise gift!);
+    }
+    return thankYouMessages;
+  }let names = ["Charlie", "Samip", "Ali"];
+  let  thankYouMessages = writeCards(names);
+  
+  console.log(thankYouMessages);
